19bf5aa540e3e9a6b278ada213ad28ab96f7bd3c
misa198 <csnsjh@gmail.com>
2020-07-30 17:30:14 +0700

    fix

--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -20,6 +20,14 @@ const images = [
   {
     uri: 'https://instagram.fhan2-2.fna.fbcdn.net/v/t51.2885-15/fr/e15/p1080x1080/115824710_4685711718109314_4802704472827200547_n.jpg?_nc_ht=instagram.fhan2-2.fna.fbcdn.net&_nc_cat=106&_nc_ohc=aZsqq9vRoqgAX_jri9i&oh=e0a3ba5f47bada7246029d4ff9b690ef&oe=5F4B0021',
     id: '23621011060892340923'
+  },
+  {
+    uri: 'https://instagram.fhan2-2.fna.fbcdn.net/v/t51.2885-15/fr/e15/p1080x1080/115824710_4685711718109314_4802704472827200547_n.jpg?_nc_ht=instagram.fhan2-2.fna.fbcdn.net&_nc_cat=106&_nc_ohc=aZsqq9vRoqgAX_jri9i&oh=e0a3ba5f47bada7246029d4ff9b690ef&oe=5F4B0021',
+    id: '23621011060892340926'
+  },
+  {
+    uri: 'https://instagram.fhan2-2.fna.fbcdn.net/v/t51.2885-15/fr/e15/p1080x1080/115824710_4685711718109314_4802704472827200547_n.jpg?_nc_ht=instagram.fhan2-2.fna.fbcdn.net&_nc_cat=106&_nc_ohc=aZsqq9vRoqgAX_jri9i&oh=e0a3ba5f47bada7246029d4ff9b690ef&oe=5F4B0021',
+    id: '23621011060892340928'
   }
 ]
 
