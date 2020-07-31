8d836f4d8ea578fba1c008bc4d8a2328dde14716
misa198 <csnsjh@gmail.com>
2020-08-01 05:59:52 +0700

    process stories uri

--- src/utils/urisProcess.ts
+++ src/utils/urisProcess.ts
@@ -1,6 +1,8 @@
 import { mediumTypes } from "../types/index";
 
-const process = async (data: any): Promise<Array<mediumTypes>> => {
+export const processPostUri = async (
+  data: any
+): Promise<Array<mediumTypes>> => {
   let rawData: Array<any> = [];
 
   // eslint-disable-next-line @typescript-eslint/no-unused-expressions
@@ -12,7 +14,8 @@ const process = async (data: any): Promise<Array<mediumTypes>> => {
   if (rawData.length !== 0) {
     rawData.forEach((ele) => {
       uris.push({
-        uri: ele.node.display_resources[2].src,
+        uri:
+          ele.node.display_resources[ele.node.display_resources.length - 1].src,
         id: ele.node.id,
         is_video: ele.node.is_video,
         video_url: ele.node.video_url,
@@ -20,7 +23,10 @@ const process = async (data: any): Promise<Array<mediumTypes>> => {
     });
   } else {
     uris.push({
-      uri: data.graphql.shortcode_media.display_resources[2].src,
+      uri:
+        data.graphql.shortcode_media.display_resources[
+          data.graphql.shortcode_media.display_resources.length - 1
+        ].src,
       id: data.graphql.shortcode_media.id,
       is_video: data.graphql.shortcode_media.is_video,
       video_url: data.graphql.shortcode_media.video_url,
@@ -30,4 +36,34 @@ const process = async (data: any): Promise<Array<mediumTypes>> => {
   return uris;
 };
 
-export default process;
+export const processStoriesUri = async (
+  data: any
+): Promise<Array<mediumTypes>> => {
+  let rawData: Array<any> = [];
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+  data.data.reels_media.length !== 0
+    ? (rawData = data.data.reels_media[0].items)
+    : [];
+
+  const uris: Array<mediumTypes> = [];
+  if (rawData.length !== 0) {
+    rawData.forEach((ele) => {
+      ele.is_video
+        ? uris.push({
+            uri: ele.display_resources[ele.display_resources.length - 1].src,
+            id: ele.id,
+            is_video: ele.is_video,
+            video_url: ele.video_resources[ele.video_resources.length - 1].src,
+          })
+        : uris.push({
+            uri: ele.display_resources[ele.display_resources.length - 1].src,
+            id: ele.id,
+            is_video: ele.is_video,
+            video_url: undefined,
+          });
+    });
+  }
+
+  return uris;
+};
