8490c9b0abc05a378ee6e21aef6073e1d6038cc5
misa198 <csnsjh@gmail.com>
2020-08-01 06:21:35 +0700

    fix

--- src/configs/regex.ts
+++ src/configs/regex.ts
@@ -2,6 +2,6 @@ export const urlPostRegex = new RegExp(
   "(?:(?:http|https)://)?(?:www.)?(?:instagram.com|instagr.am)/p/([A-Za-z0-9-_]+)"
 );
 
-export const storyPostRegex = new RegExp(
+export const urlStoryRegex = new RegExp(
   "(?:(?:http|https)://)?(?:www.)?(?:instagram.com|instagr.am)/stories/([A-Za-z0-9-_]+)"
 );
